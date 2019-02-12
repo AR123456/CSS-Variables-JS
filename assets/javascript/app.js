1edccc4ca7a5c94175a6280e1465dd1dc8b61f90
AR123456 <coloradoz99@gmail.com>
2019-02-12 05:52:23 -0700

    love calculator

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+//Love Calculator
+prompt("What is your name ? ");
+prompt("What is the name of your hearts desire ? ");
+function LoveNum() {
+  // var n = Math.random();
+
+  // n = Math.floor(n * 100) + 1;
+
+  var n = Math.floor(Math.random() * 100 + 1);
+  alert("Your love score is:  " + n + "%");
+}
+LoveNum();
