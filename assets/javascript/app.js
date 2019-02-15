002a720ab3ebe071d6d74d80b54d96f25201138d
AR123456 <coloradoz99@gmail.com>
2019-02-15 04:33:49 -0700

    99 bottles while loop

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,12 @@
-//Love Calculator
-prompt("What is your name ? ");
-prompt("What is the name of your hearts desire ? ");
-function LoveNum() {
-  // var n = Math.random();
-
-  // n = Math.floor(n * 100) + 1;
-
-  var n = Math.floor(Math.random() * 100 + 1);
-  alert("Your love score is:  " + n + "%");
+var numberOfBottles = 99;
+while (numberOfBottles >= 0) {
+  var bottleWord = "bottle";
+  if (numberOfBottles === 1) {
+    bottleWord = "bottles";
+  }
+  console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
+  console.log(numberOfBottles + " " + bottleWord + " of beer,");
+  console.log("Take one down, pass it around,");
+  numberOfBottles--;
+  console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
 }
-LoveNum();
